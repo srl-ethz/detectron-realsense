33fe78f84b0ba975f1accd45d699803822d9c9bf
Erik Bauer <erbauer@ethz.ch>
2022-05-23 12:14:55 +0200

    added point cloud extraction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from tracemalloc import start
 import detectron2
 from detectron2.utils.logger import setup_logger
+
+from pointcloud import GraspCandidate
 setup_logger()
 
 import numpy as np
@@ -19,13 +20,16 @@ import zmq
 import utils
 from logger import Logger
 from frame_transformations import transform_frame_EulerXYZ
+from pointcloud import GraspCandidate
 import detection_msg_pb2
 
+
 SHOW_WINDOW_VIS = True
-SEND_OUTPUT = True
-TARGET_OBJECT = 'person'
+SEND_OUTPUT = False
+TARGET_OBJECT = 'bottle'
 
 cam = RSCamera()
+grasp = GraspCandidate()
 
 output = cv2.VideoWriter(utils.VIDEO_FILE, cv2.VideoWriter_fourcc(
             'M', 'J', 'P', 'G'), 10, (cam.width, cam.height))
@@ -97,27 +101,36 @@ while True:
         mask_array = np.moveaxis(mask_array, 0, -1)
         mask_array_instance = []
         
+        # Loop over instances that have been detected
         for i in range(num_instances):
+
             class_idx = detected_class_idxs[i]
             bbox = pred_boxes[i].to('cpu')
             class_name = class_catalog[class_idx]
             [(xmin, ymin, xmax, ymax)] = bbox
             # cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)
+            xmin = int(xmin)
+            ymin = int(ymin)
+            xmax = int(xmax)
+            ymax = int(ymax)
 
-            center_x = int((xmax - xmin)/2 + xmin)
-            center_y = int((ymax - ymin)/2 + ymin)
+            center_x = (xmax - xmin)//2 + xmin
+            center_y = (ymax - ymin)//2 + ymin
 
             if center_y < cam.height and center_x < cam.width:
-                        depth = depth_frame[int(center_y), int(
-                            center_x)].astype(float)
+                        depth = depth_frame[center_y,
+                            center_x].astype(float)
                         distance = depth * cam.depth_scale
             else:
                 # No valid distance found
                 distance = 0.0
+                print('invalid coordinates')
 
 
             # Get translation vector relative to the camera frame
             tvec = cam.deproject(cam_intrinsics, center_x, center_y, distance)
+            # Realsense y-axis points down by default
+            tvec[1] = -tvec[1]
 
             mask_array_instance.append(mask_array[:, :, i:(i+1)])
             obj_mask = np.zeros_like(frame)
@@ -126,13 +139,25 @@ while True:
 
             
             if class_name == TARGET_OBJECT:
-                msg = detection_msg_pb2.Detection()
-                # Align camera frame with standard motion capture frame
                 if SEND_OUTPUT:
+                    # Align camera frame with standard motion capture frame
                     camera_point = [tvec[2], tvec[0], tvec[1]]
                     translation = [quad_pose.x(), quad_pose.y(), quad_pose.z()]
                     rotation = [quad_pose.roll(), quad_pose.pitch(), quad_pose.yaw()]
                     tvec = transform_frame_EulerXYZ(rotation, translation, camera_point) 
+                
+                msg = detection_msg_pb2.Detection()
+
+                # masked_frame = cv2.bitwise_and(frame, obj_mask)
+
+                crop_frame = frame[ymin:ymax, xmin:xmax, :]
+                crop_depth_frame = depth_frame[ymin:ymax, xmin:xmax]
+                cv2.rectangle(depth_colormap, (xmin, ymin), (xmax, ymax), (0,255,0))
+                cv2.imsave('depth.png', )
+                grasp.set_point_cloud_from_aligned_frames(crop_frame, crop_depth_frame, cam_intrinsics)
+                # grasp.set_point_cloud_from_aligned_frames(frame, depth_frame, cam_intrinsics)
+                grasp.save_pcd(f'pcd/pointcloud_{TARGET_OBJECT}_{utils.RECORD_COUNTER}.pcd')
+
                 msg.x = tvec[0]
                 msg.y = tvec[1]
                 msg.z = tvec[2]
---
+++ pointcloud.py
@@ -0,0 +1,43 @@
+import open3d as o3d
+import numpy as np
+import cv2
+import pyrealsense2 as rs2
+
+class GraspCandidate:
+    def __init__(self):
+        self.pointcloud = o3d.geometry.PointCloud()
+
+    def set_point_cloud_from_aligned_frames(self, frame, depth_frame, cam_intrinsics):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img_depth = o3d.geometry.Image(depth_frame)
+        img_color = o3d.geometry.Image(frame)
+
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(img_color, img_depth, convert_rgb_to_intensity=False)
+        intrinsics = o3d.camera.PinholeCameraIntrinsic(cam_intrinsics.width, cam_intrinsics.height, cam_intrinsics.fx, cam_intrinsics.fy, cam_intrinsics.ppx, cam_intrinsics.ppy)
+        pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsics)
+        pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+        self.pointcloud.points = pcd.points
+        self.pointcloud.colors = pcd.colors
+
+    def save_pcd(self, file):
+        o3d.io.write_point_cloud(file, self.pointcloud)
+
+
+    def visualise_pcd(self, file):
+        vis = o3d.visualization.Visualizer()
+        vis.create_window('PCD', width=1280, height=720)
+        pcd = o3d.io.read_point_cloud(file)
+        vis.add_geometry(pcd)
+        while True:
+            try:
+                vis.poll_events()
+                vis.update_renderer()
+            except KeyboardInterrupt:
+                vis.destroy_window()
+                del vis
+
+
+if __name__=='__main__':
+    grasp = GraspCandidate()
+    grasp.visualise_pcd('pcd/pointcloud_bottle_46.pcd')
+        
